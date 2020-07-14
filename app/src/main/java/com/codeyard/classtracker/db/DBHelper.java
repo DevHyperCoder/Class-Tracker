49398ce33340f4d75ce9cdd530b1bef9ed28f2ce
DevHyperCoder <arduinoleo88@gmail.com>
2020-07-14 14:20:59 +0530

    Added Documentation for DBHelper.java

--- app/src/main/java/com/codeyard/classtracker/db/DBHelper.java
+++ app/src/main/java/com/codeyard/classtracker/db/DBHelper.java
@@ -12,10 +12,19 @@ import java.util.List;
 
 public class DBHelper {
 
+    /**
+     * Gets all the lectures from the Database
+     * @return List of Lecture Models
+     */
     public static List<LectureModel> getAllLectures() {
         return LectureModel.listAll(LectureModel.class);
     }
 
+    /**
+     * Initialises the Database
+     * Uses SchemaGenerator to create the db and prevents any runtime errors
+     * @param context Context
+     */
     public static void initDb(Context context){
         SugarContext.init(context);
         SchemaGenerator schemaGenerator = new SchemaGenerator(context);
