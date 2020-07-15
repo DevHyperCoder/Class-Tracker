7f884632c787ff0317d110ccf9ec880ec907fb5e
DevHyperCoder <arduinoleo88@gmail.com>
2020-07-15 08:24:21 +0530

    Updated UI. More functional Added DateTimeModel.java

--- app/src/main/java/com/codeyard/classtracker/AddLectureFragment.java
+++ app/src/main/java/com/codeyard/classtracker/AddLectureFragment.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.codeyard.classtracker.models.DateTimeModel;
 import com.codeyard.classtracker.models.LectureModel;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
@@ -17,6 +18,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,14 +33,12 @@ public class AddLectureFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     final String TAG = AddLectureFragment.class.getName();
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private EditText txtTime;
+    private TextView txtTime;
     private EditText txtClass;
     private Button btnSave;
     private TextView txtWarnTime;
     private TextView txtWarnLecture;
-    private TextView daySpinner;
+    private TextView dateTextView;
     private TimePickerDialog timePickerDialog;
     private DatePickerDialog datePickerDialog;
 
@@ -50,16 +50,12 @@ public class AddLectureFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AddLectureFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AddLectureFragment newInstance(String param1, String param2) {
+    public static AddLectureFragment newInstance() {
         AddLectureFragment fragment = new AddLectureFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -67,10 +63,6 @@ public class AddLectureFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -85,13 +77,17 @@ public class AddLectureFragment extends Fragment {
         btnSave = view.findViewById(R.id.btnSave);
         txtWarnTime = view.findViewById(R.id.txtWarnTime);
         txtWarnLecture = view.findViewById(R.id.txtWarnLecture);
-        daySpinner = view.findViewById(R.id.editTextDate);
+        dateTextView = view.findViewById(R.id.dateTextView);
 
-        daySpinner.setOnClickListener(view14 -> {
+        DateTimeModel dateTimeModel = new DateTimeModel();
+
+        dateTextView.setOnClickListener(view14 -> {
             datePickerDialog = DatePickerDialog.newInstance((view141, year, monthOfYear, dayOfMonth) -> {
-                Log.d(TAG, "onDateSet: " + year);
-                Log.d(TAG, "onDateSet: " + monthOfYear);
-                Log.d(TAG, "onDateSet: " + dayOfMonth);
+                String dateString = "";
+                dateTimeModel.setDate(year, dayOfMonth, monthOfYear);
+                dateString = dateTimeModel.getDateString();
+
+                dateTextView.setText(dateString);
             }, Calendar.getInstance());
             datePickerDialog.show(requireFragmentManager(), TAG);
         });
@@ -101,6 +97,14 @@ public class AddLectureFragment extends Fragment {
                 Log.d(TAG, "onTimeSet: " + hourOfDay);
                 Log.d(TAG, "onTimeSet: " + minute);
                 Log.d(TAG, "onTimeSet: " + second);
+
+                String timeString = "";
+
+                dateTimeModel.setTime(hourOfDay,minute,second);
+                timeString=dateTimeModel.getTimeString();
+
+                txtTime.setText(timeString);
+
             }, false);
             timePickerDialog.show(requireFragmentManager(), TAG);
         });
@@ -112,19 +116,21 @@ public class AddLectureFragment extends Fragment {
             int monthOfYear = datePickerDialog.getSelectedDay().getMonth();
             int dayOfMonth = datePickerDialog.getSelectedDay().getDay();
 
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Calendar.HOUR_OF_DAY, hour);
-            calendar.set(Calendar.MINUTE, minute);
-            calendar.set(Calendar.SECOND, second);
-            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-            calendar.set(Calendar.MONTH, monthOfYear);
-            calendar.set(Calendar.YEAR, year);
 
-            Date date = calendar.getTime();
+            dateTimeModel.setDate(year, dayOfMonth, monthOfYear);
+            dateTimeModel.setTime(hour, minute, second);
+
+            Date date = dateTimeModel.getDate();
+
             String name = txtClass.getText().toString();
 
             LectureModel lectureModel = new LectureModel(name, date);
             lectureModel.save();
+
+            FragmentManager fragmentManager = getFragmentManager();
+            if (fragmentManager != null) {
+                fragmentManager.popBackStackImmediate();
+            }
 
         });
         return view;
---
+++ app/src/main/java/com/codeyard/classtracker/models/DateTimeModel.java
@@ -0,0 +1,102 @@
+package com.codeyard.classtracker.models;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateTimeModel {
+
+    private int hour;
+    private int minute;
+    private int second;
+    private int year;
+    private int monthOfYear;
+    private int dayOfMonth;
+    private Calendar calendar;
+
+    /**
+     * Constructor
+     * Initialises the calender instance
+     */
+    public DateTimeModel() {
+        calendar = Calendar.getInstance();
+    }
+
+    /**
+     * @return Date object with the params given in setDate method and setTime method
+     */
+    public Date getDate() {
+        return calendar.getTime();
+    }
+
+
+    /**
+     * Sets time using the provided parameters
+     * Sets internal Calender
+     *
+     * @param hour   hour
+     * @param minute minute
+     * @param second second
+     */
+    public void setTime(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+        setCalenderWithTime();
+    }
+
+    /**
+     * Sets date using the provided parameters
+     * Sets internal Calender
+     *
+     * @param year  year
+     * @param month month
+     * @param day   day
+     */
+    public void setDate(int year, int day, int month) {
+        this.year = year;
+        this.monthOfYear = month;
+        this.dayOfMonth = day;
+        setCalenderWithDate();
+    }
+
+
+    /**
+     * Formatting pattern: yyyy-mm-dd
+     *
+     * @return Internal Date variable formatted to string
+     */
+    public String getDateString() {
+        DateFormat dateFormat = new
+                SimpleDateFormat("yyyy-mm-dd"
+                , Locale.getDefault());
+
+        return dateFormat.format(getDate());
+    }
+
+    /**
+     * Formatting pattern: hh:mm:ss
+     *
+     * @return Internal Date variable formatted to String
+     */
+    public String getTimeString() {
+        DateFormat dateFormat = new SimpleDateFormat("hh:mm:ss", Locale.getDefault());
+        return dateFormat.format(getDate());
+    }
+
+
+    public void setCalenderWithDate() {
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, monthOfYear);
+    }
+
+    public void setCalenderWithTime() {
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, second);
+    }
+
+}
