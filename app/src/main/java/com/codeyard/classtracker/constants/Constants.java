c8c5aee5084bb938d88ad07cc2d7162b72e25eb8
DevHyperCoder <arduinoleo88@gmail.com>
2020-07-19 14:38:46 +0530

    Updated Constants to include params for Notification

--- app/src/main/java/com/codeyard/classtracker/constants/Constants.java
+++ app/src/main/java/com/codeyard/classtracker/constants/Constants.java
@@ -4,4 +4,11 @@ public class Constants {
     public static int ITEM_NORMAL = 1;
     public static int ITME_HEADER = 2;
 
+    public static String NOTIFICATION_TIME_BEFORE = "NOTIFICATION_TIME_BEFORE";
+    public static int NOTIFICATION_TIME_BEFORE_DEFAULT = 15;
+
+    public static String LECTURE_TITLE = "LECTURE_TITLE";
+    public static String LECTURE_DESC = "LECTURE_DESC";
+
+
 }
